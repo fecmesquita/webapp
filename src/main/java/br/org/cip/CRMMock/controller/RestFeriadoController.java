fd431685938977b4011c95cdf6fda1da96a561a5
Felipe de Campos Mesquita <teste>
2018-08-21 11:37:03 -0300

    Rest API para consultar Feriado

--- src/main/java/br/org/cip/CRMMock/controller/RestFeriadoController.java
+++ src/main/java/br/org/cip/CRMMock/controller/RestFeriadoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.org.cip.CRMMock.Fabric.ChaincodeService;
 import br.org.cip.CRMMock.model.Feriado;
 import br.org.cip.CRMMock.service.FeriadoService;
 
@@ -15,6 +16,14 @@ import br.org.cip.CRMMock.service.FeriadoService;
 public class RestFeriadoController {
 
 	@Autowired
+	private ChaincodeService chaincodeService;
+	
+	@RequestMapping(value="/rest/feriado/consultar/{key}", method=RequestMethod.GET)
+	public @ResponseBody Feriado findByDate(@PathVariable String key) {
+	    return chaincodeService.getFeriado(key);
+	}
+	
+	/*@Autowired
 	private FeriadoService feriadoService;
 	
 	@RequestMapping(value="/rest/feriado/consultar/{id}", method=RequestMethod.GET)
@@ -23,11 +32,14 @@ public class RestFeriadoController {
 	}
 	//curl --header "Accept: application/json" http://localhost:8080/CRMMock/rest/feriado/consultar/1
 	
-	@RequestMapping(value="/rest/feirado/alterar", method=RequestMethod.PUT)
+	
+	//retornar true -- se existir feriado (Nacional) e ativo --  ou false caso contrário. 
+	
+	/*@RequestMapping(value="/rest/feriado/alterar", method=RequestMethod.PUT)
 	public @ResponseBody Feriado updateFoo(@RequestBody Feriado feriado) {
 	    return feriadoService.alterar(feriado);
 	}
 	//curl -i -X PUT -H "Content-Type: application/json" -d '{"id":"1","descricao":"Teste Curl rest","data":"06/08/2018","situacao":"Ativo(a)","tipoFeriado":"Estadual","tipoRequisicao":"Efetivar"}' http://localhost:8080/CRMMock/rest/feriado/alterar
-	
+	*/
 	
 }
